2b1aa1d5abb7e6d6d40882b1df1a6391badbca9f
sanju50201 <sanjithk50201@gmail.com>
2023-09-26 16:24:35 +0530

    Wrote a simple Flask app to see how the model performs

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
+
 def save_object(file_path, obj):
 	try:
 		dir_path = os.path.dirname(file_path)
@@ -43,6 +45,15 @@ def evaluate_model(X_train, X_test, y_train, y_test, models, params, cv=3, n_job
 			report[list(models.keys())[i]] = test_model_score
 
 		return report
+
+	except Exception as e:
+		raise CustomException(e, sys)
+
+
+def load_object(file_path):
+	try:
+		with open(file_path, "rb") as f:
+			return dill.load(f)
 
 	except Exception as e:
 		raise CustomException(e, sys)
